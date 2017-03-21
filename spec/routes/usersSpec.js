2614b463d47c0029af553cfee6df238df90efacf
LE GROIGNEC Nicolas <nicolas.le-groignec@hotmail.fr>
2017-03-21 15:42:25 +0100

    Adding the services folder test, moar test on routers/usersSpec

--- spec/routes/usersSpec.js
+++ spec/routes/usersSpec.js
@@ -1,13 +1,13 @@
-const UsersCtrl = require('../../routes/users');
+const UsersCtrl = require('../../services/users');
 
 
 describe("UsersCtrl", () => {
     describe("=> register", () => {
-        it("Should have a valid email and a valid password", next => {
+        it("Should have an email and a password", next => {
             const req = {
                 query: {
                     email: 'test@test.me',
-                    password: 'password'
+                    password: 'P4ssw0r#'
                 }
             };
             const res = {
@@ -15,8 +15,7 @@ describe("UsersCtrl", () => {
                     return {
                         render: (view, data) => {
                             expect(code).toBe(200);
-                            expect(data.success).toBe('true');
-
+                            expect(data.success).toBeTruthy();
                             next();
                         }
                     }
@@ -26,94 +25,68 @@ describe("UsersCtrl", () => {
             UsersCtrl.index(req, res);
         });
 
-        it("Should have a valid email", next => {
-            let validEmails = [
-                'test@test.me',
-                'tEsT@test.me',
-                'test.test@test.me',
-                'Test.exAmple+teSt@test.me',
-                'test.test-exAmple@test.me',
-                'a@test.me',
-                'example-test@test-test.me',
-                'tes@s.solutions',
-                'tes@s.solutions2',
-                'tes@s.s0lutions',
-                '#!$%&\'*+-/=?^_`{}|~@example.org',
-                '1234567890123456789012345678901234567890123456789012345678901234+@example.com',
-                'test@422test.me',
-            ];
+        it("Should have an email and no password", next => {
+            const req = {
+                query: {
+                    email: 'test@test.me'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_PASSWORD_EMPTY');
+                            next();
+                        }
+                    }
+                }
+            };
 
-            for (let email of validEmails ) {
-                let isValid = UsersCtrl.isValidEmail(email);
-                expect(isValid).toBeTruthy();
-            }
-
+            UsersCtrl.index(req, res);
         });
 
-        it("Should not have a valid email", next => {
-            let invalidEmails = [
-                'te st@test.me',
-                'te"st@test.me',
-                'te"st"@test.me',
-                '.test@test.me',
-                'test.@test.me',
-                'te..st@test.me',
-                'a"b(c)d,e:f;g<h>i[j\\k]l@example.com',
-                'test@test-example.me',
-                'te%st@test.me',
-                'test@123.me',
-                'test@test',
-                'test@test@test',
-                'test.test',
-                '1234567890123456789012345678901234567890123456789012345678901234+X@example.com',
-                '',
-            ];
+        it("Should have no email but a password", next => {
+            const req = {
+                query: {
+                    password: 'P4ssw0r#'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_EMAIL_EMPTY');
+                            next();
+                        }
+                    }
+                }
+            };
 
-            for (let email of invalidEmails ) {
-                let isValid = UsersCtrl.isValidEmail(email);
-                expect(isValid).toBeFalsy();
-            }
+            UsersCtrl.index(req, res);
         });
 
-        it("Should have a valid password", next => {
-            let validPasswords = [
-                'aA1!0',
-                'P4ssw0r#',
-                'P@ss4',
-                '#az9M',
-                '0O1l!',
-                '!@?#4Aa',
-                '!@?#4AbcdEfghijKlmnOpqrSTUVwxyz',
-                '!@?#1234567890Aa',
-            ];
+        it("Should have no email and no password", next => {
+            const req = {
+                query: {}
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_EMAIL_PASSWORD_EMPTY');
+                            next();
+                        }
+                    }
+                }
+            };
 
-            for (let password of validPasswords ) {
-                let isValid = UsersCtrl.isValidPassword(password);
-                expect(isValid).toBeTruthy();
-            }
-
-        });
-
-        it("Should have a valid password", next => {
-            let invalidPasswords = [
-                'aA170',
-                'P4ssw0rD',
-                'P@sse',
-                '#az9m',
-                '0O1l!',
-                '!@?#aAa',
-                '!@?=4Aa',
-                '!@)#4Aa',
-                '!.;:§^$ù*èéâà€&~¨+-_*/µ?¤#4Aa',
-                '!@?#4AbcdEfghijKlmnOpqrSTUVwxyz',
-                '!@?#1234567890A',
-            ];
-
-            for (let password of invalidPasswords ) {
-                let isValid = UsersCtrl.isValidPassword(password);
-                expect(isValid).toBeFalsy();
-            }
-
+            UsersCtrl.index(req, res);
         });
     });
 });
