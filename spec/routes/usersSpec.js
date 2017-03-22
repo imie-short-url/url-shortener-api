cdf9bbb7fd2fd5fa668e28b6bf038b23fb2501e7
LE GROIGNEC Nicolas <nicolas.le-groignec@hotmail.fr>
2017-03-22 09:36:13 +0100

    Adding test on the users login

--- spec/routes/usersSpec.js
+++ spec/routes/usersSpec.js
@@ -86,7 +86,165 @@ describe("UsersCtrl", () => {
                 }
             };
 
-            UsersCtrl.index(req, res);
+            UsersCtrl.register(req, res);
+        });
+    });
+
+    describe("=> login", () => {
+        it("Should have an email and a password", next => {
+            const req = {
+                query: {
+                    email: 'test@test.me',
+                    password: 'P4ssw0r#'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeTruthy();
+                            expect(data.token).toBe('JWTtoken');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have an email and no password", next => {
+            const req = {
+                query: {
+                    email: 'test@test.me'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_EMAIL_AND_PASSWORD_REQUIRED');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have no email but a password", next => {
+            const req = {
+                query: {
+                    password: 'P4ssw0r#'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_EMAIL_AND_PASSWORD_REQUIRED');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have no email and no password", next => {
+            const req = {
+                query: {}
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_EMAIL_AND_PASSWORD_REQUIRED');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have invalid password", next => {
+            const req = {
+                query: {
+                    email: 'test@test.me'
+                    password: 'Pass'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_BAD_CREDENTIAL');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have invalid email", next => {
+            const req = {
+                query: {
+                    email: 'test',
+                    password: 'P4ssw0r#'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_BAD_CREDENTIAL');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
+        });
+
+        it("Should have invalid email and password", next => {
+            const req = {
+                query: {
+                    email: 'test',
+                    password: 'Pass'
+                }
+            };
+            const res = {
+                status: (code) => {
+                    return {
+                        render: (view, data) => {
+                            expect(code).toBe(200);
+                            expect(data.success).toBeFalsy();
+                            expect(data.message).toBe('ERR_BAD_CREDENTIAL');
+                            next();
+                        }
+                    }
+                }
+            };
+
+            UsersCtrl.login(req, res);
         });
     });
 });
